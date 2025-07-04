aa5531e6601736be92e5f20da59b5b8aa22efa09
Rabia Yavuluel <153555184+103105116107@users.noreply.github.com>
2025-07-04 09:50:42 +0300

    Update App.jsx

--- qr_menu2/src/App.jsx
+++ qr_menu2/src/App.jsx
@@ -1,41 +1,39 @@
 import React, { useState } from 'react';
-import './App.css'; // Genel uygulama stilleriniz
+import './App.css'; 
 import BottomNavBar from './components/BottomNavBar';
 import DessertsPage from './components/DessertsPage/DessertsPage';
 import CategoriesPage from './components/CategoriesPage/CategoriesPage';
 import HotDrinksPage from './components/HotDrinksPage/HotDrinksPage';
 import CommentsPage from './components/CommentsPage/CommentsPage';
-import MakeCommentPage from './components/MakeCommentPage/MakeCommentPage'; // CommentPage adını MakeCommentPage olarak değiştirdik
+import MakeCommentPage from './components/MakeCommentPage/MakeCommentPage';
 
 const App = () => {
-  // Başlangıçta ana sayfayı göster
+
   const [activeTab, setActiveTab] = useState('home');
 
-  // Bu fonksiyon, tab değiştiğinde veya sayfa değişimi gerektiğinde çağrılır
   const handleTabChange = (tab) => {
     setActiveTab(tab);
   };
 
   return (
     <div className="app">
-      {/* activeTab'a göre hangi bileşenin render edileceğini belirliyoruz */}
+      {}
       {activeTab === 'home' && <CategoriesPage onCategoryClick={handleTabChange} />}
       {activeTab === 'desserts' && <DessertsPage />}
       {activeTab === 'hot-drinks' && <HotDrinksPage />}
 
-      {/* Yorumlar sayfası: Eğer 'comment' sekmesindeysek CommentsPage'i göster */}
+      {}
       {activeTab === 'comment' && (
-        // CommentsPage'e, yorum yapma sayfasına geçiş için bir prop iletiyoruz
+      
         <CommentsPage onMakeCommentClick={() => handleTabChange('make-comment')} />
       )}
 
-      {/* Yorum Yapma sayfası: Eğer 'make-comment' sekmesindeysek MakeCommentPage'i göster */}
+      {}
       {activeTab === 'make-comment' && (
-        // MakeCommentPage'e, yorum gönderildikten veya geri dönüldükten sonra CommentsPage'e dönmek için bir prop iletiyoruz
         <MakeCommentPage onGoBack={() => handleTabChange('comment')} />
       )}
 
-      {/* Alt navigasyon çubuğu her zaman görünür olacak */}
+      {}
       <BottomNavBar activeTab={activeTab} onTabChange={handleTabChange} />
     </div>
   );
